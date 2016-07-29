03f32c234a6b454af59853fd59818045280fa5dd
Patrick Smith <pgwsmith@gmail.com>
2016-07-29 13:40:33 +1000

    Allow on- event handlers with <Seed>

--- src/Seed.js
+++ src/Seed.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import styler from './all';
 
-export default function Seed({ Component = 'div', ...otherProps }) {
+const handlerKeyRegex = /^on[A-Z0-9]/
+
+// Pick props matching with on- prefix
+function pickHandlers(props) {
+	return Object.keys(props).reduce(((picked, key) => {
+		if (handlerKeyRegex.test(key)) {
+			picked[key] = props[key]
+		}
+		return picked
+	}))
+}
+
+export default function Seed({ Component = 'div', ...props }) {
 	// `children` gets handled automatically by styler
 	return (
-		<Component { ...styler(otherProps) } />
+		<Component { ...pickHandlers(props) } { ...styler(props) } />
 	)
 }
